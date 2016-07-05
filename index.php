5be615b2d316cc9016727ce891687b2a02d8f24e
Youri Maratkanov <fenix_63@mail.ru>
2016-07-06 00:09:46 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script><![endif]-->
+	<title></title>
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<link href="style.css" rel="stylesheet">
+	<link href="select-box.css" rel="stylesheet">
+
+
+   <link href="font/opensans/opensans.css" rel="stylesheet">
+   <link href="font/hattori/hattori.css" rel="stylesheet">
+</head>
+<body>
+   <div id="wrapper">
+      <div id="header">
+         <div class="bg_top">
+         	<div class="container">
+         		<div class="geo">
+         		</div>
+         		<div class="country">
+
+					<div class="new-select-style-wpandyou">
+		         			<select>
+		         				<option>Россия</option>
+		         				<option>Белоруссия</option>
+		         				<option>Казахстан</option>
+		         				<option>Украина</option>
+		         			</select>
+		         	</div>
+         		</div>
+         		<div class="phone"></div>
+         		<div class="mail"></div>
+
+
+
+         	</div>
+         </div>
+         <!--<div class="geo"></div>-->
+         <div class="bg_menu">
+         	<div class="container">
+         		 <div class="link1"><a href="#">О магазине</a></div>
+		         <div class="link2"><a href="#o2">О препарате</a></div>
+		         <div class="link3"><a href="#o3">Преимущества</a></div>
+		         <div class="link4"><a href="#o4">Гарантии</a></div>
+		         <div class="link5"><a href="#o5">Спецпредложение</a></div>
+		         <div class="link6"><a href="#o6">Вопросы и ответы</a></div>
+		         <div class="link7"><a href="#delivery">Доставка</a></div>
+		         <div class="link8"><a href="#contacts">Контакты</a></div>
+         	</div>
+
+         </div>
+         
+         <div class="mail2"></div>
+         
+         <div class="phone2"></div>
+
+         
+         <div class="header">
+         	<div class="container">
+         		<div class="tong"></div>
+         		<div class="price"></div>
+         		<div class="text04"><b>1499 </b>руб.</div>
+         	</div>
+         </div>	
+         
+         
+         
+
+         
+         <div class="border"></div>
+         <div class="galka"></div>
+         <div class="bg_text"></div>
+         <div class="galka2"></div>
+         <div class="bg_text2"></div>
+         <div class="galka3"></div>
+         <div class="bg_text3"></div>
+         <div class="t_a_p">ТОНГКАТ АЛИ ПЛАТИНУМ</div>
+         <div class="text02">Средство для улучшения потенции</div>
+         <div class="text03">Для повышения потенции, эрекции и лечение половых дисфункций</div>
+         
+         <div class="text05">100% гарантия результата</div>
+         <div class="text06">Эффективен в любом возрасте</div>
+         <div class="text07">Не содержит химических субстанций</div>
+         <div class="bg_arrow"></div>
+         <div class="text08">Успейте сделать заказ до 30 июля 2016 года и получите энергетический браслет в подарок</div>
+         <div class="text09"></div>
+         <div class="text10">Заполните заявку и мы <b>бесплатно доставим</b> Вам Тонгкат в любой город России</div>
+         <div class="text11">Доставка строго конфиденциальна</div>
+         <div class="text12">Имя:</div>
+         <div class="text13">Номер телефона:</div>
+         <div class="text14">E-mail:</div>
+         <div class="text15">Улучшает кровообращение в органах малого таза.</div>
+         <div class="text16">Стимулирует иммунитет и общее самочувствие, снимает умственную и физическую усталость, повышает работоспособность.</div>
+         <div class="text17">Рекомендуется при инфекциях мочеполых путей (уретриты, циститы, пиелонефриты), простатите, фибромиоме матки, бесплодии.</div>
+         <div class="text18">Тонгкат Али Платинум не содержит химических субстанций.</div>
+         <div class="text19">Эффективен в любом возрасте.</div>
+         <div class="text20">Уникальный процесс производства гарантирует его расщепление и полное всасывание биологически активных веществ.</div>
+         <div class="text21">Клинические исследования показали значительное повышение сексуальной активности, наступающее в течении 1 часа после приема препарата.</div>
+         <div class="text22">Эффект сохраняется 3-7 дней.</div>
+         <div class="text23">Регулярный прием по 1 капсуле в 1-3 дня приводит к стойкому улучшению потенции и сексуальных возможностей.</div>
+         <div class="text24"><a name="o2" class="o2"></a><b>Натуральный препарат на основе корня  Тонгкат Али Платинум</b></div>
+         <div class="text25"><a name="o4" class="o4"></a><b>Тонгкат Али Платинум даёт 100% результат</b></div>
+         <div class="text26"><b>Триэтаноламин</b></div>
+         <div class="text27"><b>Продолжительность</b></div>
+         <div class="text28"><b>Моментальный эффект </b></div>
+         <div class="text29"> Усиливает желание и укрепляет эрекцию </div>
+         <div class="text30">Продлевает половой акт </div>
+         <div class="text31"> Увеличивает половую чувствительность и усиливает оргазм  </div>
+         <div class="text32">Энергетический браслет Power Balance</div>
+         <div class="text33"><a name="o5" class="o5"></a><b>Подарок при заказе до 30.06.2016</b></div>
+         <div class="text34">Заполните заявку и мы <b>бесплатно доставим</b> Вам Тонгкат в любой город России</div>
+         <div class="text35">Доставка строго конфиденциальна</div>
+         <div class="text36">Имя:</div>
+         <div class="text37">Номер телефона:</div>
+         <div class="text38">E-mail:</div>
+         <div class="text39" onclick="showSpoiler(this);"></div>
+         <div class="text40"><a name="o3" class="o3"></a><b>Наши преимущества</b></div>
+         <div class="text41">100% гарантия качества. Только оригинальная продукция. Работа напрямую с производителем</div>
+         <div class="text42">Вы ничем не рискуете: оплата заказа при получении или другие удобные для Вас способы оплаты</div>
+         <div class="text43">Низкие цены Хорошие скидки и акции с подарками при заказе</div>
+         <div class="text44">Доставка по всей России Пункты самовывоза в 100 городах</div>
+         <div class="text45">Быстрая доставка заказанного товара удобным способом</div>
+         <div class="text46">Качественная поддержка клиентов - круглосуточный колл-центр</div>
+         <div class="text47">Низкая стоимость доставки по городам России -  от 200 руб.</div>
+         <div class="text48">Быстре и удобное оформление заказа за 1 клик через сайт</div>
+         <div class="text49"><b>Также рекомендуем приобрести</b></div>
+         <div class="text50"><b>Эффективный гель Hot Glide Liquid Pleasure на водной основе обладает экстра увлажнаяющими свойствами и обеспечивает длительное нежное скольжение</b></div>
+         <div class="text51">Для анального и вагинального секса</div>
+         <div class="text52"><b>600</b> руб.</div>
+         <div class="text53">Классическая смазка, вагинальгая</div>
+         <div class="text54">Германия</div>
+         <div class="text55">Водная</div>
+         <div class="text56">30</div>
+         <div class="text57">Страна</div>
+         <div class="text58">Тип</b></div>
+         <div class="text59">Состав</b></div>
+         <div class="text60">Объем, мл</b></div>
+         <div class="text61">Характеристики Hot Glide Liquid Pleasure, 30 мл</div>
+         <div class="text62">Устраняет неприятные ощущения</div>
+         <div class="text63">Совместима с презервативами</div>
+         <div class="text64">Подходит для использования с игрушками</div>
+         <div class="text65">Без вкуса и запаха</div>
+         <div class="text66"><a name="o6" class="o6"></a>Вопросы и ответы</div>
+         <div class="text67"><b>1499 </b>руб.</div>
+         <div class="text68">Тонгкат Али Платинум © 2016 Все права защищены </div>
+         <div class="text69">Успей заказать до 30 июля 2016 года и получить подарок!</div>
+         <div class="text70">Сертификаты</div>
+         <div class="text71">Посмотри, как Анна Семенович выбирает успешных мужчин! </div>
+         <div class="text72">?</div>
+         <div class="text73">?</div>
+         <div class="text74">?</div>
+         <div class="text75">?</div>
+         <div class="text76">?</div>
+         <div class="text77">?</div>
+         <div class="video"><iframe width="560" height="315" src="https://www.youtube.com/embed/6Ic4OuOIkmQ" frameborder="0" allowfullscreen></iframe></div>
+         <div class="icon_zamok"></div>
+         <div class="icon_zamok_2"></div>
+         <div class="bg_zamok"></div>
+         <div class="bg_zamok_2"></div>
+         <div id="popup">
+             <div class="exit" onclick="openbox();"></div>
+             <div class="popupphoto"></div>
+             <div class="popuptext"></div>
+         </div>
+         <div class="noactive"></div>
+         <div class="noactive2"></div>
+         <div class="noactive3"></div>
+         <div class="noactive4"></div>
+         <div class="noactive5"></div>
+         <div class="noactive6"></div>
+         <div class="active"></div>
+         <div class="bg"></div>
+         <div class="bg_2"></div>
+         <div class="bg_3"></div>
+         <div class="bg_4"></div>
+         <div class="bg_5"></div>
+         <div class="bg_6"></div>
+         <div class="bg_7"></div>
+         <div class="bg_8"></div>
+         <div class="bg_9"></div>
+         <div class="sertificat"></div>
+         <div class="sertificat2"></div>
+         <div class=""></div>
+         <div class=""></div>
+         <div class=""></div>
+         <div class="tong_bottom"></div>
+         <div class="fig"></div>
+         <div class="fig2"></div>
+         <div class="fig3"></div>
+         <div class="fig4"></div>
+         <div class="fig5"></div>
+         <div class="galka_2_1"></div>
+         <div class="galka_2_2"></div>
+         <div class="galka_2_3"></div>
+         <div class="galka_2_4"></div>
+         <div class="galka_2_5"></div>
+         <div class="star"></div>
+         <div class="star2"></div>
+         <div class="star3"></div>
+         <div class="star4"></div>
+         <div class="star5"></div>
+         <div class="star6"></div>
+         <div class="star7"></div>
+         <div class="star8"></div>
+         <div class="normal2"></div>
+         <div class="normal3"></div>
+         <div class="normal4" onclick="openbox();"></div>
+         <div class="normal5"></div>
+         <div class="normal6"></div>
+		 <div class="delivery" id="delivery">
+		 	<a href="#">Доставка</a>
+		 </div>
+
+		 <div class="contacts" id="contacts">
+		 	<a href="#">Контакты</a>
+		 </div>
+
+         <!--<div class="russian">РОССИЯ</div>-->
+         <div class="_1"></div>
+         <div class="_2"></div>
+         <div class="_3"></div>
+         <div class="pic_1"></div>
+         <div class="pic_2"></div>
+         <div class="pic_3"></div>
+         <div class="tongat"></div>
+         <div class="arrow"></div>
+         <div class="arrow2"></div>
+         <div class="arrow3"></div>
+          <div class="arrow_3"></div>
+         <div class="arrow4"></div>
+         <div class="arrow5"></div>
+         <div class="arrow6"></div>
+         <div class="arrow7"></div>
+         <div class="arrow8"></div>
+         <div class="arrow9"></div>
+         <div class="arrow10"></div>
+         <div class="arrow11"></div>
+         <div class="icon"></div>
+         <div class="icon2"></div>
+         <div class="icon3"></div>
+         <div class="photo"></div>
+         <div class="photo2"></div>
+         <div class="price2"></div>
+         <div class="price3"></div>
+         <div class="present"></div> 
+         
+         
+         <div class="field1"><input class="fields" "type"="text" name="name"></div>
+         <div class="field2"><input class="fields" "type"="text" name="number"></div>
+         <div class="field3"><input class="fields" "type"="text" name="email"></div>
+         
+         <div class="field4"><input class="fields" "type"="text" name="name"></div>
+         <div class="field5"><input class="fields" "type"="text" name="number"></div>
+         <div class="field6"><input class="fields" "type"="text" name="email"></div>
+         
+         
+      </div>
+   </div>
+</body>
+
+<script type="text/javascript">
+function openbox() {
+    var div = document.getElementById('popup');
+    if(div.style.display == 'block') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.display = 'block';
+    }
+  }
+</script>
+<body>
+
+</script>
+</html>
+
